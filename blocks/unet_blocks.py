d6fc589216e537921431d51810af3d827378b7f4
avansp <avan.sp@gmail.com>
2020-04-12 22:59:38 +0100

    FEATURE: added get_output_shape method

--- blocks/unet_blocks.py
+++ blocks/unet_blocks.py
@@ -1,5 +1,7 @@
 from torch import nn
 import torch
+from collections import OrderedDict
+import utils.layer_utils as lu
 import torch.nn.functional as F
 
 
@@ -14,14 +16,14 @@ class DoubleConvBlock(nn.Module):
                  kernel_size=3, stride=1):
         super(DoubleConvBlock, self).__init__()
 
-        self.model = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
+        self.model = nn.Sequential(OrderedDict([
+            ('conv1', nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride)),
+            ('norm1', nn.BatchNorm2d(out_channels)),
+            ('relu1', nn.ReLU(inplace=True)),
+            ('conv2', nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=stride)),
+            ('norm2', nn.BatchNorm2d(out_channels)),
+            ('relu2', nn.ReLU(inplace=True))
+        ]))
 
         self.out_image = None
 
@@ -31,6 +33,13 @@ class DoubleConvBlock(nn.Module):
 
     def get_output(self):
         return self.out_image
+
+    def get_output_shape(self, height, width):
+        models = {k: v for k, v in self.model.named_children()}
+        out_shape = lu.conv2d_output_shape(height, width, models['conv1'])
+        out_shape = lu.conv2d_output_shape(out_shape[0], out_shape[1], models['conv2'])
+
+        return out_shape
 
 
 class DownSampleBlock(nn.Module):
@@ -53,6 +62,13 @@ class DownSampleBlock(nn.Module):
 
     def get_output(self):
         return self.model[1].get_output()
+
+    def get_output_shape(self, height, width):
+        modules = list(self.model.children())
+        out_shape = lu.maxpool2d_output_shape(height, width, modules[0])
+        out_shape = modules[1].get_output_shape(out_shape[0], out_shape[1])
+
+        return out_shape
 
 
 class UpSampleBlock(nn.Module):
@@ -84,3 +100,4 @@ class UpSampleBlock(nn.Module):
         out_image = self.double_conv(out_image)
 
         return out_image
+
