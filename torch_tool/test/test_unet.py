7eeabb6b366e2a10547465e61394b4adf53d28f1
Avan <avan.sp@gmail.com>
2020-06-25 11:46:13 +0100

    FIX: corrected test_unet according to the new layout

--- torch_tool/test/test_unet.py
+++ torch_tool/test/test_unet.py
@@ -10,15 +10,18 @@ class UNetUnitTest(unittest.TestCase):
 
     def test_double_conv_block(self):
         block = DoubleConvBlock(3, 2).to(self.device)
-        # out_shape = block.get_output_shape(572, 572)
-        # self.assertEqual(out_shape, (568, 568))
+        x = torch.zeros(2, 3, 572, 572, device=self.device)
+        z = block(x)
+        self.assertEqual(z.shape, (2, 2, 568, 568))
 
     def test_down_sample_block(self):
         block = DownSampleBlock(3, 2).to(self.device)
-        # out_shape = block.get_output_shape(568, 568)
-        # self.assertEqual(out_shape, (280, 280))
+        x = torch.zeros(2, 3, 568, 568, device=self.device)
+        z = block(x)
+        self.assertEqual(z.shape, (2, 2, 280, 280))
 
     def test_vanilla_unet(self):
         vu = VanillaUNet(3, 2).to(self.device)
-        # out_shape = vu.get_output_shape(572, 572)
-        # self.assertEqual(out_shape, (388, 388))
+        x = torch.zeros(2, 3, 572, 572, device=self.device)
+        z = vu(x)
+        self.assertEqual(z.shape, (2, 2, 388, 388))
